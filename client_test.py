7db0575169d236501e498bc85e1eb8825688a6cd
Ar3mis22 <aryashirgaonkar42@gmail.com>
2023-11-13 00:02:05 +0530

    Task 1 done

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,25 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getDataPoint_noTopBid(self):
+        quote = {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'id': '0.109974697771', 'stock': 'XYZ'}
+        self.assertEqual(getDataPoint(quote), (quote['stock'], None, quote['top_ask']['price'], None))
+
+  def test_getDataPoint_noTopAsk(self):
+        quote = {'top_bid': {'price': 120.48, 'size': 109}, 'timestamp': '2019-02-11 22:06:30.572453', 'id': '0.109974697771', 'stock': 'XYZ'}
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], None, None))
+
+  def test_getDataPoint_noTopBidAndAsk(self):
+        quote = {'timestamp': '2019-02-11 22:06:30.572453', 'id': '0.109974697771', 'stock': 'XYZ'}
+        self.assertEqual(getDataPoint(quote), (quote['stock'], None, None, None))
+
+
 
 
 
